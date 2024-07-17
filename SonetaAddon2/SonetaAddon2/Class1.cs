2b76d883c8971b410cbccccf810ae80ac4d19c25
Twardyy <gajdowskihubert@gmail.com>
2024-07-17 15:39:06 +0200

    Soneta

---
+++ SonetaAddon2/SonetaAddon2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Limilabs.Mail;
+using Soneta.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SonetaAddon2;
+using Soneta.Business.UI;
+
+[assembly: Worker(typeof(SonetaAddon2.Class1), typeof(Soneta.Handel.DokHandlowe))]
+
+namespace SonetaAddon2
+{
+    public class Class1
+    {
+
+        [Action("Test", Target = ActionTarget.Menu, Priority = 10002)]
+        public object xdd()
+        {
+            return new MessageBoxInformation("TEST", "test1");
+        }
+    }
+}
